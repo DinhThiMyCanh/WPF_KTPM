7ff926da58f625a76a01e44603b38e325a1476f7
canhdhqn <canhdhqn@gmail.com>
2023-04-04 10:33:35 +0700

    Thêm Data binding và Style

---
+++ WPF_KTPM/Data_Binding.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WPF_KTPM
+{
+    /// <summary>
+    /// Interaction logic for Data_Binding.xaml
+    /// </summary>
+    public partial class Data_Binding : Window
+    {
+        public Data_Binding()
+        {
+            InitializeComponent();
+            Product A = new Product(){
+                Id = 1,
+                Name = "Quyển Sách"
+            };
+            this.DataContext = A;
+        }
+    }
+}
---
+++ WPF_KTPM/Product.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPF_KTPM
+{
+    class Product
+    {
+        private int id;
+        private string name;//Ctrl+R+E
+
+        public int Id { get => id; set => id = value; }
+        public string Name { get => name; set => name = value; }
+    }
+}
